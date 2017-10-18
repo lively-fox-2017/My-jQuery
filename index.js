b6557ca1d58fff08f53bff345ea6493da5852787
harynp <harynugrahaputra@gmail.com>
2017-10-18 18:08:54 +0700

    My-jquery Email

---
+++ index.js
@@ -0,0 +1,23 @@
+function validateEmail(email) {
+  var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+}
+
+function validate() {
+  $("#result").text("");
+  var email = $("#email").val();
+  if (validateEmail(email)) {
+    $("#result").text(email + " is valid");
+    $("#result").css("color", "green");
+    $("#email").css("color", "green");
+    $(".warna").css("border","2px solid green")
+  } else {
+    $("#result").text(email + " is not valid");
+    $("#result").css("color", "red");
+    $("#email").css("color", "red");
+    $(".warna").css("border","2px solid red")
+  }
+  return false;
+}
+
+$("#email").keyup("click", validate);
